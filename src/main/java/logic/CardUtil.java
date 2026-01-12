c0c65738de66acf5fe1adc4d6879a50774d13b17
pasin-buakhaw <pasinbuakhaw@gmail.com>
2026-01-12 12:35:33 +0700

    full game

--- src/main/java/logic/CardUtil.java
+++ src/main/java/logic/CardUtil.java
@@ -1,5 +1,5 @@
 package logic;
-
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -9,39 +9,71 @@ public class CardUtil {
 	
 	public static boolean isExistsInList(UnitCard card, ArrayList<UnitCard> list) {
 		
-		//TODO: Fill Code
+		for(UnitCard a_card : list){
+			if(a_card.equals(card)){return true;}
+		}
 		return false;
 
 	}
 	
 	public static boolean isExistsInList(UnitDeck deck, ArrayList<UnitDeck> list) {
-		
-		//TODO: Fill Code
+
+		for(UnitDeck a_deck : list){
+			if(a_deck.equals(deck)){return true;}
+		}
 		return false;
 
 	}
-	
+
 	public static boolean cardExistsInDeckList(ArrayList<UnitDeck> deckList, UnitCard cardToTest) {
-		
-		//TODO: Fill Code
+
+		for (UnitDeck deck : deckList) {
+
+			if (deck.existsInDeck(cardToTest)) {
+				return true;
+			}
+		}
 		return false;
 	}
-	
-	public static ArrayList<UnitCard> getCardsFromFile(String filename){
-		
+
+	public static ArrayList<UnitCard> getCardsFromFile(String filename) {
+
 		ArrayList<UnitCard> cardsFromFile = new ArrayList<UnitCard>();
 		InputStream inputStream = CardUtil.class.getClassLoader().getResourceAsStream(filename);
-          	if (inputStream == null) {
-                	System.out.println("Cannot find file!");
-                	return null;
-            	}
 
-            	Scanner myReader = new Scanner(inputStream);
+		if (inputStream == null) {
+			System.out.println("Cannot find file!");
+			return null;
+		}
 
+		Scanner myReader = new Scanner(inputStream);
 
-		//TODO: Fill Code below
-		
-		return null;
+		// TODO: Fill Code below
+		try {
+			while (myReader.hasNextLine()) {
+				String line = myReader.nextLine();
+				String[] dataArray = line.split(",");
+				ArrayList<String> cardData = new ArrayList<String>(Arrays.asList(dataArray));
+				String name = cardData.get(0);
+				int bloodCost = Integer.parseInt(cardData.get(1));
+				int power = Integer.parseInt(cardData.get(2));
+				int health = Integer.parseInt(cardData.get(3));
+				String flavorText = cardData.get(4);
+
+				UnitCard newCard = new UnitCard(name, bloodCost, power, health, flavorText);
+
+				cardsFromFile.add(newCard);
+			}
+		} catch (Exception e) {
+
+			System.out.println("File contains string with incorrect format!");
+			return null;
+		} finally {
+
+			myReader.close();
+		}
+
+		return cardsFromFile;
 	}
 
 	public static void printCardList(ArrayList<UnitCard> cardList, boolean verbose) {
---
+++ src/main/java/logic/UnitDeck.java
@@ -0,0 +1,88 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class UnitDeck {
+    private ArrayList<CardCounter> cardsInDeck ;
+    private String deckName;
+
+
+    public UnitDeck( String deckName) {
+        setDeckName(deckName);
+        this.cardsInDeck = new ArrayList<CardCounter>();
+
+    }
+
+    public void setCardsInDeck(ArrayList<CardCounter> cardsInDeck) {
+        this.cardsInDeck = cardsInDeck;
+    }
+
+    public void setDeckName(String deckName) {
+        this.deckName = deckName;
+    }
+
+    public ArrayList<CardCounter> getCardsInDeck() {
+        return cardsInDeck;
+    }
+
+    public String getDeckName() {
+        return deckName;
+    }
+    public void addCard(UnitCard newCard, int count) {
+
+        if (count <= 0) {
+            return;
+        }
+        for (CardCounter counter : cardsInDeck) {
+
+            if (counter.getCard().equals(newCard)) {
+
+                counter.setCount(counter.getCount() + count);
+                return;
+            }
+        }
+        CardCounter newCounter = new CardCounter(newCard, count);
+        cardsInDeck.add(newCounter);
+    }
+
+    public void removeCard(UnitCard toRemove, int count) {
+
+        if (count <= 0) return;
+
+
+        for (int i = 0; i < cardsInDeck.size(); i++) {
+            CardCounter counter = cardsInDeck.get(i);
+            if (counter.getCard().equals(toRemove)) {
+
+                int newCount = counter.getCount() - count;
+                counter.setCount(newCount);
+
+
+                if (counter.getCount() <= 0) {
+                    cardsInDeck.remove(i);
+                }
+                return;
+            }
+        }
+    }
+    public int cardCount() {
+        int total = 0;
+        for (CardCounter counter : cardsInDeck) {
+            total += counter.getCount();
+        }
+        return total;
+    }
+    public boolean existsInDeck(UnitCard card) {
+        for (CardCounter counter : cardsInDeck) {
+            if (counter.getCard().equals(card) && counter.getCount() >= 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean equals(UnitDeck other) {
+        return this.deckName.equals(other.getDeckName());
+    }
+}
+
+
