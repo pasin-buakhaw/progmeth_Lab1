b525dd48b3f2437e69c35025d1913860108fc4aa
pasin-buakhaw <pasinbuakhaw@gmail.com>
2026-01-12 10:42:01 +0700

    Unit card & Card counter

--- src/main/java/logic/CardCounter.java
+++ src/main/java/logic/CardCounter.java
@@ -8,5 +8,21 @@ public class CardCounter {
 	public String toString() {
 		return  this.getCard() + " x " + this.getCount();
 	}
+	public CardCounter(UnitCard card,int count){
+		this.card = card;
+		this.count = count;
+	}
+	public void setCount(int count){
+		this.count = Math.max(0,count);
+	}
+
+
+	public UnitCard getCard(){
+		return  this.card;
+	}
+
+	public int getCount(){
+		return this.count;
+	}
 
 }
--- src/main/java/logic/UnitCard.java
+++ src/main/java/logic/UnitCard.java
@@ -10,4 +10,49 @@ public class UnitCard {
 	public String toString() {
 		return this.getName() + " (POW: " + this.getPower() + ", HP: " + this.getHealth() + ")";
 	}
+
+	public  UnitCard(String name,int bloodCost,int power ,int health,String flavorText){
+		this.name = name;
+		this.bloodCost = bloodCost;
+		this.power = power;
+		this.health = health;
+		this.flavorText = flavorText;
+	}
+
+	public  void setName(String newName){
+		if(newName.isBlank()){
+			this.name = "Creature";
+		}else {
+			this.name = newName;
+		}
+	}
+	public  void setBloodCost(int newBloodCost){
+		this.bloodCost = Math.max(0, newBloodCost);
+	}
+
+	public  void setPower(int NewPower){
+		this.power = Math.max(0, NewPower);
+	}
+	public  void setHealth(int NewHealth){
+		this.health = Math.max(1, NewHealth);
+
+	}
+	public void  setFlavorText(String text){
+		this.flavorText = text;
+	}
+
+
+	public String getName(){
+		return  this.name;
+	}
+	public int getBloodCost(){return  this.bloodCost;}
+	public  int getPower(){return  this.power;}
+	public  int getHealth(){return  this.health;}
+	public  String getFlavorText(){return this.flavorText;}
+
+	public boolean equals(UnitCard other){
+
+		return  this.name.equals(other.getName());
+
+	}
 }
